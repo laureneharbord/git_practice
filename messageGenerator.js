d73ca03f6a87872f053ea6518f4cda858fea7246
L Harbord <81684478+laureneharbord@users.noreply.github.com>
2021-03-31 15:28:36 +0100

    variables added

---
+++ messageGenerator.js
@@ -0,0 +1,3 @@
+let starsign = ['Capricorn','Aquarius','Aries','Pices','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagitarius'];
+let focus = ['strength','power','communication','breathing'];
+let worry = ['love life', 'financial trouble','career','smelly toes'];
