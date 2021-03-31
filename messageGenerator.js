5665a12ff3592972fae100dbb00d129ee6fdd7c4
L Harbord <81684478+laureneharbord@users.noreply.github.com>
2021-03-31 15:55:21 +0100

    function defined and called

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,3 +1,10 @@
 let starsign = ['Capricorn','Aquarius','Aries','Pices','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagitarius'];
 let focus = ['strength','power','communication','breathing'];
 let worry = ['love life', 'financial trouble','career','smelly toes'];
+const getRandom = (array) => {
+return(array[Math.floor(Math.random()*array.length)])
+}
+
+let newHoroscope = 'Wow! it looks like your moon is in ' + getRandom(starsign) +'. Take time to focus on your ' + getRandom(focus) + " and don't worry about " + getRandom(worry) + ', it will get easier.';
+
+console.log(newHoroscope);
